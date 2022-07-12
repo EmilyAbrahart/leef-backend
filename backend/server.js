7416fac02462e592921a68e29f39b34b75346f49
EmilyAbrahart <emilyabrahart@gmail.com>
2022-07-12 02:42:27 +0100

    cors config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5001;
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5001;
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
